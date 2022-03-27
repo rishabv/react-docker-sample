e7890a076a20856f9a866cfc9c0a67476267e29c
RishabhvermaV <rishabh.verma@appventurez.com>
2022-03-27 21:10:59 +0530

    dev and prod docker images done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,125 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  Tooltip,
+  BarChart,
+  Bar,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Legend,
+} from "recharts";
 
 function App() {
+  const data = [
+    {
+      projectedProfit: 150000,
+      AnswerRef: "one",
+      Text: "Year 1",
+      Score: 0,
+      RespondentPercentage: 12,
+      Rank: 1,
+      color: "#96d3e3",
+    },
+    {
+      projectedProfit: 250000,
+      AnswerRef: "two",
+      Text: "Year 2",
+      Score: 0,
+      RespondentPercentage: 32,
+      Rank: 2,
+      color: "#6bafc2",
+    },
+    {
+      projectedProfit: 500000,
+      AnswerRef: "three",
+      Text: "Year 3",
+      Score: 1,
+      RespondentPercentage: 41,
+      Rank: 3,
+      color: "#017fb1",
+    },
+    {
+      projectedProfit: 750000,
+      AnswerRef: "four",
+      Text: "Year 4",
+      Score: 0,
+      RespondentPercentage: 16,
+      Rank: 4,
+      color: "#01678e",
+    },
+    {
+      projectedProfit: 1000000,
+      AnswerRef: "five",
+      Text: "Year 5",
+      Score: 0,
+      RespondentPercentage: 23,
+      Rank: 2,
+      color: "#015677",
+    },
+    {
+      projectedProfit: 500000,
+      AnswerRef: "three",
+      Text: "Year 3",
+      Score: 1,
+      RespondentPercentage: 41,
+      Rank: 3,
+      color: "#017fb1",
+    },
+    {
+      projectedProfit: 500000,
+      AnswerRef: "three",
+      Text: "Year 3",
+      Score: 1,
+      RespondentPercentage: 41,
+      Rank: 3,
+      color: "#017fb1",
+    },
+    {
+      projectedProfit: 500000,
+      AnswerRef: "three",
+      Text: "Year 3",
+      Score: 1,
+      RespondentPercentage: 41,
+      Rank: 3,
+      color: "#017fb1",
+    },
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BarChart
+        width={800}
+        height={400}
+        data={data}
+        margin={{ top: 20, left: -1 }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="projectedProfit" />
+        {/* <YAxis dataKey="projectedProfit"/> */}
+        <Tooltip />
+        <Bar
+          yAxisId="left"
+          dataKey="projectedProfit"
+          fill="#FF7C1C"
+          onClick={(e) => console.log(e.payload.projectedProfit)}
+        />
+        {/* <Legend/> */}
+        <Bar
+          yAxisId="right"
+          dataKey="RespondentPercentage"
+          fill="#00B4FF"
+          onClick={(e) => console.log(e.payload.RespondentPercentage)}
+        />
+        <Bar
+          yAxisId="right"
+          dataKey="Rank"
+          fill="#FF5252"
+          onClick={(e) => console.log(e.payload.Rank)}
+        />
+      </BarChart>
+
+      <p>{process.env.REACT_APP_NAME}</p>
+      {process.env.REACT_APP_URL}
     </div>
   );
 }
